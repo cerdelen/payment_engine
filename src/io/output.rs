5b9c6cfe764dbfe06665154cb32a24a8d107d7dc
Cedric Erdelen <cedric.erdelen@gmail.com>
2026-03-01 00:44:11 +0100

    feat: force print headers even if no values are generated

--- src/io/output.rs
+++ src/io/output.rs
@@ -5,6 +5,13 @@ use crate::txn_engine::engine::AccountBalances;
 pub fn print_account_balances(balances: &AccountBalances) {
     let mut writer = csv::Writer::from_writer(io::stdout());
 
+    if balances.is_empty() {
+        if let Err(e) = writer.write_record(["client", "available", "held", "total", "locked"]) {
+            eprintln!("Error: writing to stdout: {e}");
+        }
+        return;
+    }
+
     for balance in balances.values() {
         if let Err(e) = writer.serialize(balance) {
             eprintln!("Error: writing serialized account balance to stdout: {e}");
